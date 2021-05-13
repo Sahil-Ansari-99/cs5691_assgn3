41ec10fd665a62999baa5e832ea81fbaeb8f8e74
Sahil-Ansari-99 <39992186+Sahil-Ansari-99@users.noreply.github.com>
2021-05-13 22:18:48 +0530

    Added hidden layer and convergence plots.

--- dataset1b.py
+++ dataset1b.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
 from sklearn import svm
+from sklearn.neural_network._base import ACTIVATIONS
 from sklearn.metrics import confusion_matrix
 import warnings
 warnings.filterwarnings('ignore')
@@ -15,8 +16,8 @@ dev_data = pd.read_csv('data/Dataset 1B/dev.csv')
 dev_data = dev_data.to_numpy()
 
 
-def mlp(x_, y_, hidden_layers=(5, 5), activation_='relu'):
-    model_ = MLPClassifier(hidden_layer_sizes=hidden_layers, random_state=0, max_iter=250, activation=activation_)
+def mlp(x_, y_, hidden_layers=(5, 5), activation_='relu', max_iter=250):
+    model_ = MLPClassifier(hidden_layer_sizes=hidden_layers, random_state=0, max_iter=max_iter, activation=activation_)
     model_.fit(x_, y_)
     return model_
 
@@ -72,68 +73,86 @@ x_val = dev_data[:, :d_-1]
 # print('Val Confusion Matrix:')
 # print(confusion_matrix(y_val, val_pred))
 
-degrees = [2, 3, 4, 5, 6]
-cs = [0.01, 0.1, 1.0, 10]
-best_degree = 3
-best_acc = 0
-for degree in cs:
-    model = svm_model(x_data, y_data, kernel='rbf', c_=degree)
-    train_acc = model.score(x_data, y_data)
-    val_acc = model.score(x_val, y_val)
-    print(degree, 'Train Acc:', train_acc, 'Val Acc:', val_acc)
-    if val_acc >= best_acc:
-        best_acc = val_acc
-        best_degree = degree
+model = mlp(x_data, y_data, hidden_layers=(40, 40), activation_='relu', max_iter=250)
+weights = model.coefs_
+intercepts = model.intercepts_
+test_data = x_data[0]
+node_num = 3
+layer_num = 3
+# for weight in weights[:2]:
+#     test_data = np.matmul(weight.T, test_data)
+#     print(weight.shape)
 
-print('Best C:', best_degree)
-model = svm_model(x_data, y_data, kernel='rbf', c_=best_degree)
-train_pred = predict(model, x_data)
-val_pred = predict(model, x_val)
-print('Train Confusion Matrix:')
-print(confusion_matrix(y_data, train_pred))
-print('Val Confusion Matrix:')
-print(confusion_matrix(y_val, val_pred))
+# degrees = [2, 3, 4, 5, 6]
+# cs = [0.01, 0.1, 1.0, 10]
+# best_degree = 3
+# best_acc = 0
+# for degree in cs:
+#     model = svm_model(x_data, y_data, kernel='rbf', c_=degree)
+#     train_acc = model.score(x_data, y_data)
+#     val_acc = model.score(x_val, y_val)
+#     print(degree, 'Train Acc:', train_acc, 'Val Acc:', val_acc)
+#     if val_acc >= best_acc:
+#         best_acc = val_acc
+#         best_degree = degree
+#
+# print('Best C:', best_degree)
+# model = svm_model(x_data, y_data, kernel='rbf', c_=best_degree)
+# train_pred = predict(model, x_data)
+# val_pred = predict(model, x_val)
+# print('Train Confusion Matrix:')
+# print(confusion_matrix(y_data, train_pred))
+# print('Val Confusion Matrix:')
+# print(confusion_matrix(y_val, val_pred))
 # support_vectors = model.support_vectors_
 # print(model.score(x_val, y_val))
 #
 # fig, ax = plt.subplots()
-# colors = ['r', 'g', 'b', 'y']
-# legend_arr = []
-# i = 0
-# min_x = 999
-# max_x = -999
-# min_y = 999
-# max_y = -999
-# for point in x_data:
-#     if point[0] < min_x:
-#         min_x = point[0]
-#     if point[0] > max_x:
-#         max_x = point[0]
-#     if point[1] < min_y:
-#         min_y = point[1]
-#     if point[1] > max_y:
-#         max_y = point[1]
-# # ax.scatter(x, y, color=colors[i])
-#
-# x_list = np.linspace(min_x, max_x, 100)
-# y_list = np.linspace(min_y, max_y, 100)
-#
-# z = np.zeros((len(x_list), len(x_list)))
-# X = []
-# Y = []
-# for i in range(len(x_list)):
-#     temp_x = []
-#     temp_y = []
-#     for j in range(len(y_list)):
-#         temp_x.append(x_list[i])
-#         temp_y.append(y_list[j])
-#         point = np.array([x_list[i], y_list[j]])
-#         z[i][j] = predict(model, point)
-#     X.append(temp_x)
-#     Y.append(temp_y)
-#
-# ax.contourf(X, Y, z)
-#
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+colors = ['r', 'g', 'b', 'y']
+legend_arr = []
+i = 0
+min_x = 999
+max_x = -999
+min_y = 999
+max_y = -999
+for point in x_data:
+    if point[0] < min_x:
+        min_x = point[0]
+    if point[0] > max_x:
+        max_x = point[0]
+    if point[1] < min_y:
+        min_y = point[1]
+    if point[1] > max_y:
+        max_y = point[1]
+# ax.scatter(x, y, color=colors[i])
+
+x_list = np.linspace(min_x, max_x, 100)
+y_list = np.linspace(min_y, max_y, 100)
+
+z = np.zeros((len(x_list), len(x_list)))
+X = []
+Y = []
+for i in range(len(x_list)):
+    temp_x = []
+    temp_y = []
+    for j in range(len(y_list)):
+        temp_x.append(x_list[i])
+        temp_y.append(y_list[j])
+        point = np.array([x_list[i], y_list[j]])
+        b = 0
+        for weight in weights[:layer_num]:
+            point = np.matmul(weight.T, point) + intercepts[b]
+            b += 1
+            ACTIVATIONS['relu'](point)
+        # z[i][j] = predict(model, point)
+        z[i][j] = point[node_num - 1]
+    X.append(temp_x)
+    Y.append(temp_y)
+
+ax.plot_surface(X, Y, z)
+
 # x = []
 # y = []
 # for point in x_data:
@@ -141,7 +160,7 @@ print(confusion_matrix(y_val, val_pred))
 #     y.append(point[1])
 #     ax.scatter(x, y, color='k')
 #     i += 1
-#
+
 # ax.scatter(support_vectors[:, 0], support_vectors[:, 1], color='r')
-#
-# plt.show()
+
+plt.show()
